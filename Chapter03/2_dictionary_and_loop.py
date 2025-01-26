b9a8b37e97e0c79ce5986539a6b4f25f164f6ff0
choseongchan3712 <choseongchan3712@naver.com>
2025-01-26 23:34:22 +0900

    2_dictionary_and_loop

--- Chapter03/2_dictionary_and_loop.py
+++ Chapter03/2_dictionary_and_loop.py
@@ -26,10 +26,10 @@ print(dict_b["director"])
 # ['안소니 루소', '조 루소']
 
 dictionary = {
-  "name": "7D 건조 망고",
-  "type": "당절임",
-  "ingredient": ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
-  "origin": "필리핀"
+    "name": "7D 건조 망고",
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
+    "origin": "필리핀",
 }
 
 print("name:", dictionary["name"])
@@ -49,3 +49,48 @@ print("name:", dictionary["name"])
 # ? 딕셔너리 안에있는 리스트도 인덱스를 지정하여 특정 값을 받아올 수 있음
 print(dictionary["ingredient"][1])
 # 설탕
+
+
+# ! (3) 딕셔너리에 값 추가하기/제거하기
+# ? 딕셔너리[새로운 키] = 새로운 값
+
+dictionary["price"] = 5000
+print(dictionary)
+# {'name': '8D 건조 망고', 'type': '당절임', 'ingredient': ['망고', '설탕', '메타중아황산나트륨', '치자황색소'], 'origin': '필리핀', 'price': 5000}
+
+# ? 딕셔너리 요소 제거는 리스트 처럼 del키워드를 사용하여 특정 키를 지정하면 해당 요소가 제거됨.
+del dictionary["ingredient"]
+print(dictionary)
+# {'name': '8D 건조 망고', 'type': '당절임', 'origin': '필리핀', 'price': 5000}
+
+
+# ! (4) 딕셔너리 내부에 키가 있는지 확인하기
+# ? 1) in 키워드
+dictionary = {
+    "name": "7D 건조 망고",
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
+    "origin": "필리핀",
+}
+
+print("name" in dictionary)
+# True
+print("value" in dictionary)
+# False
+
+# ? 2) get() 함수는 딕셔너리의 키로 값을 추출하는 기능으로, 존재하지 않는 키에 접근할 경우 None을 출력함.
+print(dictionary.get("name"))
+# 7D 건조 망고
+print(dictionary.get("value"))
+# None
+
+
+# ! (5) for 반복문: 딕셔너리와 함께 사용하기
+# ? for 키 변수 in 딕셔너리: 코드
+
+for key in dictionary:
+    print(key, ":", dictionary[key])
+# name : 7D 건조 망고
+# type : 당절임
+# ingredient : ['망고', '설탕', '메타중아황산나트륨', '치자황색소']
+# origin : 필리핀
